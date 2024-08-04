053a2ffdb084c6137ddc7c61451e67bcf3fbf223
KaspianDev <kacperleaguenine@gmail.com>
2024-08-04 21:35:55 +0200

    Initial commit

---
+++ src/main/java/com/github/kaspiandev/nbtgui/NBTGui.java
@@ -0,0 +1,17 @@
+package com.github.kaspiandev.nbtgui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NBTGui extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
